da4caf9498fcd8be5e93e33c456f9a75bf8262d6
Fabricio <fabricio.jacob@outlook.com>
2020-12-13 21:28:15 -0300

    add

--- transacao/src/main/java/com/github/fabriciolfj/transacao/domain/model/TransacaoMessage.java
+++ transacao/src/main/java/com/github/fabriciolfj/transacao/domain/model/TransacaoMessage.java
@@ -12,6 +12,6 @@ import lombok.NoArgsConstructor;
 public class TransacaoMessage {
 
     private String transactionId;
-    private String customerId;
+    private String serviceName;
     private String status;
 }
